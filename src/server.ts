d31dffabaea8cc35efdf26cbeca79e6c35eda5b7
nicholaslouback <nilouback@hotmail.com>
2025-07-09 10:52:48 -0300

    chore: adding dependencies and libraries

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { fastify } from "fastify";
+import { 
+  type ZodTypeProvider, 
+  serializerCompiler, 
+  validatorCompiler 
+} from "fastify-type-provider-zod";
+
+import { fastifyCors } from "@fastify/cors";
+import { env } from "./env.ts"; 
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:3000'
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get('/health', () => {
+  return 'OK'
+})
+
+app.listen({ port: env.PORT})
